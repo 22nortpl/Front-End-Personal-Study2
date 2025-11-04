ec2906e40ac249e321e653caf38be8221d254112
22nortpl <minsanglee03@gmail.com>
2025-11-04 13:27:22 +0900

    week3 assignments

---
+++ week3/src/myprofile/Pros.js
@@ -0,0 +1,18 @@
+function Pros() {
+
+ return (
+
+ <div>
+
+ <h2>장점</h2>
+
+ <p>저의 장점이라면, 열정, 끈기, 그리고 사람 말을 잘 들어주는 다정함입니다.</p>
+
+ </div>
+
+ );
+
+}
+
+
+export default Pros;
---
+++ week3/src/myprofile/Rapper.js
@@ -0,0 +1,18 @@
+function Rapper() {
+
+ return (
+
+ <div>
+
+ <h2>Painful But Grateful</h2>
+
+ <p>저는 2025년 9얼 24일 정식으로 싱글 앨범을 내고 데뷔하였습니다.</p>
+
+ </div>
+
+ );
+
+}
+
+
+export default Rapper;
